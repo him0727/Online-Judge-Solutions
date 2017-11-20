c305130a2d6836d9d3736a620c2296eb0545ab34
him0727 <him0727@hotmail.com>
2017-11-21 00:19:43 +0800

    Create SPCU.cpp

---
+++ SPOJ/SPCU.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define ll long long int
+ 
+using namespace std;
+ 
+int main() {
+  int total;
+  scanf("%d", &total);
+  
+  while (total--) {
+    ll n;
+    bool wrong = false;
+    scanf("%lli", &n);
+    ll *heights;
+    heights = (ll*) malloc(n * sizeof(ll));
+    
+    for (ll i = 0; i < n; i++) {
+      scanf("%lli", &heights[i]);
+      if (heights[i] > i) wrong = true;
+    }
+    
+    if (wrong) {
+      printf("NO\n");
+    } else {
+      printf("YES\n");
+    }
+  }
+  return 0;
+} 
