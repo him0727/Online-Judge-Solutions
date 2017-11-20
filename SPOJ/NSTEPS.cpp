a72898098e71ad8693400df487661e091cd35b80
him0727 <him0727@hotmail.com>
2017-11-21 02:13:26 +0800

    Create NSTEPS.cpp

---
+++ SPOJ/NSTEPS.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+  int t;
+  scanf("%d", &t);
+  
+  while (t--) {
+    int x, y;
+    scanf("%d %d", &x, &y);
+    
+    if ((x == 1 && y == 1) || (x == 0 && y == 0)) {
+      printf("%d\n", x);
+    } else if (x == y || y == x - 2) {
+      if (x & 1) {
+        printf("%d\n", x + y - 1);
+      } else {
+        printf("%d\n", x + y);
+      }
+    } else {
+      printf("No Number\n");
+    }
+  }
+  return 0;
+} 
