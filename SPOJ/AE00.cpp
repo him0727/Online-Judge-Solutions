7f233c4e5eb79a9901f4b72cff62975ec995bbdf
him0727 <him0727@hotmail.com>
2017-11-24 02:01:41 +0800

    Create AE00.cpp

---
+++ SPOJ/AE00.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+  int n, ans = 0, i = 2;
+  scanf("%d", &n);
+  while (n / i + 1 - i > 0) {
+    ans += n / i + 1 - i++;
+  }
+  printf("%d", ans + n);
+  return 0;
+} 
