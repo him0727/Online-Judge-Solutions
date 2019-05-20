43ca9447f900347767fa4dd5329292dbef885611
him0727 <him0727@hotmail.com>
2019-05-20 20:20:45 +0800

    upload 20190520

--- SPOJ/My_problems/MACHCOOL_1.cpp
+++ SPOJ/My_problems/MACHCOOL_1.cpp
@@ -2,29 +2,6 @@
 
 using namespace std;
 
-const int MAX = 110;
-
-int itr;
-vector<int> g[MAX];
-vector<int> p(MAX), used(MAX);
-
-bool dfs(int u) {
-  if (u == -1) {
-    return true;
-  }
-  if (used[u] == itr) {
-    return false;
-  }
-  used[u] = itr;
-  for (int v : g[u]) {
-    if (dfs(p[v])) {
-      p[v] = u;
-      return true;
-    }
-  }
-  return false;
-}
-
 int main() {
   int tt;
   scanf("%d", &tt);
@@ -35,33 +12,10 @@ int main() {
     for (int i = 0; i < N; i++) {
       scanf("%d", &tasks[i]);
     }
-    int low = 0, high = 86400;
-    int ans;
-    while (low <= high) {
-      int mid = (low + high) / 2;
-      for (int i = 0; i < N; i++) {
-        g[i].clear();
-        used[i] = p[i] = -1;
-        for (int j = 0; j < N; j++) {
-          if (i != j && tasks[i] + mid <= tasks[j]) {
-            g[i].push_back(j);
-          }
-        }
-      }
-      itr = 0;
-      int need = 0;
-      for (int i = 0; i < N; i++) {
-        itr++;
-        if (!dfs(i)) {
-          need++;
-        }
-      }
-      if (M >= need) {
-        low = mid + 1;
-        ans = mid;
-      } else {
-        high = mid - 1;
-      }
+    sort(tasks.begin(), tasks.end());
+    int ans = 86400;
+    for (int i = 0; i + M < N; i++) {
+      ans = min(ans, tasks[i + M] - tasks[i]);
     }
     printf("%d\n", ans);
   }
--- SPOJ/My_problems/MACHCOOL_2.cpp
+++ SPOJ/My_problems/MACHCOOL_2.cpp
@@ -2,6 +2,29 @@
 
 using namespace std;
 
+const int MAX = 110;
+
+int itr;
+vector<int> g[MAX];
+vector<int> p(MAX), used(MAX);
+
+bool dfs(int u) {
+  if (u == -1) {
+    return true;
+  }
+  if (used[u] == itr) {
+    return false;
+  }
+  used[u] = itr;
+  for (int v : g[u]) {
+    if (dfs(p[v])) {
+      p[v] = u;
+      return true;
+    }
+  }
+  return false;
+}
+
 int main() {
   int tt;
   scanf("%d", &tt);
@@ -12,14 +35,33 @@ int main() {
     for (int i = 0; i < N; i++) {
       scanf("%d", &tasks[i]);
     }
-    sort(tasks.begin(), tasks.end());
-    int ans = 86400;
-    for (int i = 0; i < M; i++) {
-      int cur = 86400;
-      for (int j = i + M; j < N; j += M) {
-        cur = min(cur, tasks[j] - tasks[j - M]);
+    int low = 0, high = 86400;
+    int ans;
+    while (low <= high) {
+      int mid = (low + high) / 2;
+      for (int i = 0; i < N; i++) {
+        g[i].clear();
+        used[i] = p[i] = -1;
+        for (int j = 0; j < N; j++) {
+          if (i != j && tasks[i] + mid <= tasks[j]) {
+            g[i].push_back(j);
+          }
+        }
       }
-      ans = min(ans, cur);
+      itr = 0;
+      int need = 0;
+      for (int i = 0; i < N; i++) {
+        itr++;
+        if (!dfs(i)) {
+          need++;
+        }
+      }
+      if (M >= need) {
+        low = mid + 1;
+        ans = mid;
+      } else {
+        high = mid - 1;
+      }
     }
     printf("%d\n", ans);
   }
