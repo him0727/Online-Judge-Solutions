7e8f156b5a97de28a23ebcabe0788de2c5b2a37e
him0727 <him0727@hotmail.com>
2022-02-04 00:45:43 +0800

    swap

--- SPOJ/My_problems/MACHCOOL2_2.cpp
+++ SPOJ/My_problems/MACHCOOL2_2.cpp
@@ -1,22 +1,22 @@
 #include <bits/stdc++.h>
-
+ 
 using namespace std;
 
-bool dfs(int u, int itr, int extra, vector<pair<int, int>>& tasks, vector<vector<int>>& g, vector<int>& p, vector<int>& used) {
-  if (u == -1) {
-    return true;
+int count(int extra, vector<pair<int, int>>& tasks) {
+  int n = (int) tasks.size();
+  vector<pair<int, int>> time(n * 2);
+  for (int i = 0; i < n; i++) {
+    time[i * 2] = {tasks[i].first, 1};
+    time[i * 2 + 1] = {tasks[i].second + extra, -1};
   }
-  if (used[u] == itr) {
-    return false;
+  sort(time.begin(), time.end());
+  int cur_least = 0;
+  int least = 0;
+  for (int i = 0; i < n * 2; i++) {
+    cur_least += time[i].second;
+    least = max(least, cur_least);
   }
-  used[u] = itr;
-  for (int v : g[u]) {
-    if (tasks[v].first >= tasks[u].second + extra && dfs(p[v], itr, extra, tasks, g, p, used)) {
-      p[v] = u;
-      return true;
-    }
-  }
-  return false;
+  return least;
 }
 
 int main() {
@@ -26,8 +26,6 @@ int main() {
     int n;
     cin >> n;
     vector<pair<int, int>> tasks(n);
-    vector<vector<int>> g(n);
-    vector<int> p(n, -1), used(n, -1);
     for (int i = 0; i < n; i++) {
       cin >> tasks[i].first;
       tasks[i].second = 0;
@@ -38,38 +36,13 @@ int main() {
       }
       tasks[i].second += tasks[i].first;
     }
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
-        if (i != j && tasks[j].first >= tasks[i].second) {
-          g[i].push_back(j);
-        }
-      }
-    }
-    int itr = 0;
-    int least = 0;
-    for (int i = 0; i < n; i++) {
-      itr++;
-      if (!dfs(i, itr, 0, tasks, g, p, used)) {
-        least++;
-      }
-    }
+    int least = count(0, tasks);
     int ans = 0;
     int low = 0;
     int high = 86400;
     while (low <= high) {
       int mid = (low + high) / 2;
-      for (int i = 0; i < n; i++) {
-        used[i] = p[i] = -1;
-      }
-      itr = 0;
-      int need = 0;
-      for (int i = 0; i < n; i++) {
-        itr++;
-        if (!dfs(i, itr, mid, tasks, g, p, used)) {
-          need++;
-        }
-      }
-      if (need <= least) {
+      if (least >= count(mid, tasks)) {
         ans = mid;
         low = mid + 1;
       } else {
--- SPOJ/My_problems/MACHCOOL2_3.cpp
+++ SPOJ/My_problems/MACHCOOL2_3.cpp
@@ -1,22 +1,22 @@
 #include <bits/stdc++.h>
- 
+
 using namespace std;
 
-int count(int extra, vector<pair<int, int>>& tasks) {
-  int n = (int) tasks.size();
-  vector<pair<int, int>> time(n * 2);
-  for (int i = 0; i < n; i++) {
-    time[i * 2] = {tasks[i].first, 1};
-    time[i * 2 + 1] = {tasks[i].second + extra, -1};
+bool dfs(int u, int itr, int extra, vector<pair<int, int>>& tasks, vector<vector<int>>& g, vector<int>& p, vector<int>& used) {
+  if (u == -1) {
+    return true;
   }
-  sort(time.begin(), time.end());
-  int cur_least = 0;
-  int least = 0;
-  for (int i = 0; i < n * 2; i++) {
-    cur_least += time[i].second;
-    least = max(least, cur_least);
+  if (used[u] == itr) {
+    return false;
   }
-  return least;
+  used[u] = itr;
+  for (int v : g[u]) {
+    if (tasks[v].first >= tasks[u].second + extra && dfs(p[v], itr, extra, tasks, g, p, used)) {
+      p[v] = u;
+      return true;
+    }
+  }
+  return false;
 }
 
 int main() {
@@ -26,6 +26,8 @@ int main() {
     int n;
     cin >> n;
     vector<pair<int, int>> tasks(n);
+    vector<vector<int>> g(n);
+    vector<int> p(n, -1), used(n, -1);
     for (int i = 0; i < n; i++) {
       cin >> tasks[i].first;
       tasks[i].second = 0;
@@ -36,13 +38,38 @@ int main() {
       }
       tasks[i].second += tasks[i].first;
     }
-    int least = count(0, tasks);
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (i != j && tasks[j].first >= tasks[i].second) {
+          g[i].push_back(j);
+        }
+      }
+    }
+    int itr = 0;
+    int least = 0;
+    for (int i = 0; i < n; i++) {
+      itr++;
+      if (!dfs(i, itr, 0, tasks, g, p, used)) {
+        least++;
+      }
+    }
     int ans = 0;
     int low = 0;
     int high = 86400;
     while (low <= high) {
       int mid = (low + high) / 2;
-      if (least >= count(mid, tasks)) {
+      for (int i = 0; i < n; i++) {
+        used[i] = p[i] = -1;
+      }
+      itr = 0;
+      int need = 0;
+      for (int i = 0; i < n; i++) {
+        itr++;
+        if (!dfs(i, itr, mid, tasks, g, p, used)) {
+          need++;
+        }
+      }
+      if (need <= least) {
         ans = mid;
         low = mid + 1;
       } else {
